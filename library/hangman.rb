8e9f969c1bfe8fcf9b9f8c6edbf7aca3366e64a4
James <jnjensen@live.com>
2020-08-09 22:20:42 -0500

    starting game

--- library/hangman.rb
+++ library/hangman.rb
@@ -2,4 +2,31 @@ require "csv"
 
 puts "Let's Play Hangman!"
 
-word_list = File.readlines "5desk.txt"
+$WORD_LIST = File.readlines "5desk.txt"
+
+class Player
+    attr_reader :name
+    attr_accessor :score
+  
+    def initialize(name, score = [0, 0])
+      @name = name
+      @score = score
+    end
+end
+
+def choose_word()
+    choosen = false
+
+    until choosen
+        word = $WORD_LIST.shuffle
+        word = word[0]
+        
+        if word.length >= 5 && word.length <= 12
+            choosen = true
+        end
+    end
+
+    return word    
+end
+
+puts choose_word
